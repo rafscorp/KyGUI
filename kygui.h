5dff313cf50d5d282a529a6ca6c432263d6cc280
rafscorp <metsaccout@gmail.com>
2026-04-27 00:02:57 -0300

    kygui

---
+++ kygui.h
@@ -0,0 +1,8 @@
+//
+// Created by kylesx on 27/04/2026.
+//
+
+#ifndef KYGUI_KYGUI_H
+#define KYGUI_KYGUI_H
+
+#endif //KYGUI_KYGUI_H
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <kygui.h>
+
+int main()
+{
+
+}
