03aeeb26041bc28b5aaa7d364efd1526ae348050
RafsCorp <metsaccout@gmail.com>
2026-04-27 22:55:43 -0300

    Include X11 headers in kygui.c
    Added X11 headers for window management.

--- kygui.c
+++ kygui.c
@@ -1,6 +1,8 @@
 #include <kyGUI.h>   //Include dos headers
+#include <X11/Xlib.h>  //Includes do X11
+#include <X11/Xutil.h> //...
+#include <X11/keysym.h>  //...
 
 int ky_win_init(int w, int h, const char *title)
 {
-
 }
